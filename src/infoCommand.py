1f41eddd56b11a272117653c6426d6a360837183
Greg <gregor.armand@efrei.net>
2021-08-31 18:24:39 +0000

    [EDIT / ADD] : jsplus

--- src/infoCommand.py
+++ src/infoCommand.py
@@ -9,24 +9,24 @@ def formatString(string):
     string = string.replace("'", "")
     string = string.split(", ")
     for i in range(len(string)):
-        if i > 1:
+        if i > 1 and i != 5 :
             string[i] = int(string[i])
     return string
 
 async def printMessage(message, array):
-    embed = discord.Embed(title = "Stats", description = f"{array[1]}'s Stats.", color = 0x1D068F) #replace message.author by sql name in descritpion
+    embed = discord.Embed(title = "Stats", description = f"{array[1]}'s Stats.<:blank:877523987142438922><:blank:877523987142438922><:blank:877523987142438922><:blank:877523987142438922> {array[5]}", color = 0x1D068F) #replace message.author by sql name in descritpion
     embed.set_author(name = message.author, icon_url = message.author.avatar_url)
     embed.set_thumbnail(url = message.author.avatar_url)
     embed.add_field(name = "\u200B", value = "\u200B", inline = False)
     embed.add_field(name = ":trophy: Gearscore<:blank:877523987142438922><:blank:877523987142438922><:blank:877523987142438922>", value = array[2], inline = True)
     embed.add_field(name = ":crossed_swords: level<:blank:877523987142438922><:blank:877523987142438922><:blank:877523987142438922><:blank:877523987142438922>", value = array[3], inline = True)
     embed.add_field(name = ":medal: SP", value = array[4], inline = True)
-    embed.add_field(name = "<:red_rune:880086179968581632> Assault", value = array[5], inline = True)
-    embed.add_field(name = "<:orange_rune:880086179746291743> Control", value = array[6], inline = True)
-    embed.add_field(name = "<:yellow_rune:880086180132184064> Fate", value = array[7], inline = True)
-    embed.add_field(name = "<:blue_rune:880086179695976479> Protection", value = array[8], inline = True)
-    embed.add_field(name = "<:green_rune:880086179901480970> Support", value = array[9], inline = True)
-    embed.add_field(name = "<:purple_rune:880086179951833088> Awakening", value = array[10], inline = True)
+    embed.add_field(name = "<:red_rune:880086179968581632> Assault", value = array[6], inline = True)
+    embed.add_field(name = "<:orange_rune:880086179746291743> Control", value = array[7], inline = True)
+    embed.add_field(name = "<:yellow_rune:880086180132184064> Fate", value = array[8], inline = True)
+    embed.add_field(name = "<:blue_rune:880086179695976479> Protection", value = array[9], inline = True)
+    embed.add_field(name = "<:green_rune:880086179901480970> Support", value = array[10], inline = True)
+    embed.add_field(name = "<:purple_rune:880086179951833088> Awakening", value = array[11], inline = True)
     await message.channel.send(embed = embed)
     return 0
 
@@ -39,7 +39,7 @@ async def infoCommand(message, argv, client):
         await message.channel.send("No account registered")
         return -1
     else :
-        cursor.execute("SELECT uuid, name, u_gear, u_level, u_skill, r_red, r_orange, r_yellow, r_blue, r_green, r_purple FROM user WHERE uuid = '" + str(message.author.id) + "';")
+        cursor.execute("SELECT uuid, name, u_gear, u_level, u_skill, u_class, r_red, r_orange, r_yellow, r_blue, r_green, r_purple FROM user WHERE uuid = '" + str(message.author.id) + "';")
         result2 = formatString(cursor.fetchone())
         print("[" + str(datetime.now()) + "] : <" + str(message.author) + "> :-->", result2)
         await printMessage(message, result2)
